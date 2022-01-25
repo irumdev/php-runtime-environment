8e2db6cf5f8433ef6a932e1fb6391ded5ed49597
irumdev <jklsj1252@gmail.com>
2022-01-25 21:27:35 +0900

    Feat : 추가 Logs, dotenv 더미파일

--- infra/run.sh
+++ infra/run.sh
@@ -1,5 +1,10 @@
 #bin/bash
 
+# Make Dummy Files
+touch ../storage/logs/access.log
+touch ../storage/logs/error.log
+cp ../.env.example ../.env
+
 # Container Build & Run
 docker-compose --project-name "boilerplate" up -d
 
@@ -7,3 +12,4 @@ docker-compose --project-name "boilerplate" up -d
 containerID=$(docker-compose --project-name "boilerplate" ps -q web)
 docker exec $containerID bash -c "cd /var/www; /bin/composer.phar install"
 docker exec $containerID bash -c "cd /var/www; php artisan route:clear"
+docker exec $containerID bash -c "cd /var/www; php artisan key:generate"
