939dffc62e12e36dc0c46e59d4301893ab2a1cbd
irumdev <jklsj1252@gmail.com>
2022-01-25 21:33:05 +0900

    Feat : 변경 Project Name

--- infra/run.sh
+++ infra/run.sh
@@ -6,10 +6,10 @@ touch ../storage/logs/error.log
 cp ../.env.example ../.env
 
 # Container Build & Run
-docker-compose --project-name "boilerplate" up -d
+docker-compose --project-name "php-runtime-environment" up -d
 
 # Composer Package Install
-containerID=$(docker-compose --project-name "boilerplate" ps -q web)
+containerID=$(docker-compose --project-name "php-runtime-environment" ps -q web)
 docker exec $containerID bash -c "cd /var/www; /bin/composer.phar install"
 docker exec $containerID bash -c "cd /var/www; php artisan route:clear"
 docker exec $containerID bash -c "cd /var/www; php artisan key:generate"
